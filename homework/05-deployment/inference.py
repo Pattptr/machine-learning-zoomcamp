9b28ac4c9324e1c7efac822f7257d77d42edc142
Patteera Triamamornwooth <90516429+Pattptr@users.noreply.github.com>
2025-11-02 18:30:19 +0000

    edit homework05

--- homework/05-deployment/inference.py
+++ homework/05-deployment/inference.py
@@ -6,6 +6,12 @@ sample = {
     "annual_income": 80304.0
 }
 
+# sample =  {
+#     "lead_source": "paid_ads",
+#     "number_of_courses_viewed": 2,
+#     "annual_income": 79276.0
+# }
+
 url = "http://0.0.0.0:8000/predict"
 
 response = requests.post(url, json=sample)
